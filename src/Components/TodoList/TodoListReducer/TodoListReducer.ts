7c75ae3428971abb94c65d0d97ec19fae9f20386
Alexey2576 <alexeynikinitin@yandex.by>
2021-12-28 17:38:39 +0300

    Some styled from MUI and added error when user add new task

--- src/Components/TodoList/TodoListReducer/TodoListReducer.ts
+++ src/Components/TodoList/TodoListReducer/TodoListReducer.ts
@@ -1,6 +1,16 @@
 import {FilterType, TodoListStateType} from "../../../App";
 import {v1} from "uuid";
 
+export type DispatchType =
+   ReturnType<typeof addTodoListAC>
+   | ReturnType<typeof addTaskAC>
+   | ReturnType<typeof removeTaskAC>
+   | ReturnType<typeof removeTodoListAC>
+   | ReturnType<typeof setValueInputAddTodoListAC>
+   | ReturnType<typeof setFilterTodoListAC>
+   | ReturnType<typeof setValueSelectAC>
+   | ReturnType<typeof setCheckedTaskAC>
+
 export const todoListReducer = (state: TodoListStateType, action: DispatchType): TodoListStateType => {
    switch (action.type) {
       case "ADD_TODOLIST": {
@@ -23,6 +33,7 @@ export const todoListReducer = (state: TodoListStateType, action: DispatchType):
                ...state.tasks,
                [action.todoList_ID]: [...state.tasks[action.todoList_ID], {
                   task_ID: v1(),
+                  checked: false,
                   task_title: action.title,
                   task_priority: action.selectValue
                }]
@@ -70,48 +81,23 @@ export const todoListReducer = (state: TodoListStateType, action: DispatchType):
             } : tl)
          }
       }
+      case "SET_CHECKED_TASK": {
+         return {
+            ...state,
+            tasks: {...state.tasks,
+               [action.todoList_ID]: state.tasks[action.todoList_ID].map(t => t.task_ID === action.task_ID ? {...t, checked: action.checked} : t)}
+         }
+      }
       default:
          throw new Error("Error")
    }
 }
-export type DispatchType =
-   { type: "ADD_TODOLIST", todoList_ID: string }
-   | { type: "ADD_TASK", title: string, todoList_ID: string, selectValue: FilterType }
-   | { type: "REMOVE_TASK", todoList_ID: string, task_ID: string }
-   | { type: "REMOVE_TODOLIST", todoList_ID: string }
-   | { type: "SET_VALUE_INPUT_ADD_TODOLIST", title: string }
-   | { type: "SET_FILTER_TODOLIST", todoList_ID: string, filter: FilterType }
-   | { type: "SET_VALUE_SELECT", todoList_ID: string, selectValue: FilterType }
-export const addTodoListAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   todoList_ID: string
-) => dispatch({type: "ADD_TODOLIST", todoList_ID})
-export const addTaskAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   title: string,
-   todoList_ID: string,
-   selectValue: FilterType
-) => dispatch({type: "ADD_TASK", title, todoList_ID, selectValue})
-export const removeTaskAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   todoList_ID: string,
-   task_ID: string
-) => dispatch({type: "REMOVE_TASK", todoList_ID, task_ID})
-export const removeTodoListAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   todoList_ID: string,
-) => dispatch({type: "REMOVE_TODOLIST", todoList_ID})
-export const setValueInputAddTodoListAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   title: string,
-) => dispatch({type: "SET_VALUE_INPUT_ADD_TODOLIST", title: title})
-export const setFilterTodoListAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   todoList_ID: string,
-   filter: FilterType,
-) => dispatch({type: "SET_FILTER_TODOLIST", todoList_ID, filter})
-export const setValueSelectAC = (
-   dispatch: (dispatch: DispatchType) => void,
-   todoList_ID: string,
-   selectValue: FilterType,
-) => dispatch({type: "SET_VALUE_SELECT", todoList_ID, selectValue})
+
+export const addTodoListAC = (todoList_ID: string) => ({type: "ADD_TODOLIST", todoList_ID} as const)
+export const addTaskAC = (title: string, todoList_ID: string, selectValue: FilterType) => ({type: "ADD_TASK", title, todoList_ID, selectValue} as const)
+export const removeTaskAC = (todoList_ID: string, task_ID: string) => ({type: "REMOVE_TASK", todoList_ID, task_ID} as const)
+export const removeTodoListAC = (todoList_ID: string) => ({type: "REMOVE_TODOLIST", todoList_ID} as const)
+export const setValueInputAddTodoListAC = (title: string) => ({type: "SET_VALUE_INPUT_ADD_TODOLIST", title: title} as const)
+export const setFilterTodoListAC = (todoList_ID: string, filter: FilterType) => ({type: "SET_FILTER_TODOLIST", todoList_ID, filter} as const)
+export const setValueSelectAC = (todoList_ID: string, selectValue: FilterType) => ({type: "SET_VALUE_SELECT", todoList_ID, selectValue} as const)
+export const setCheckedTaskAC = (todoList_ID: string, task_ID: string, checked: boolean) => ({type: "SET_CHECKED_TASK", todoList_ID, task_ID, checked} as const)
