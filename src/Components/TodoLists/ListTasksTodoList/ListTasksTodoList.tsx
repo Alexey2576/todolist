f6a38db69f9eadf24002da5fd80d8782030d825e
Alexey2576 <alexeynikinitin@yandex.by>
2021-12-29 14:12:28 +0300

    Moved TitleTodoList, AddTaskTodoList, LinearProgressTasks, ListTasksTodoList from TodoList(remove) to TodoLists

---
+++ src/Components/TodoLists/ListTasksTodoList/ListTasksTodoList.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {List} from "@material-ui/core";
+import {TaskTodoList, TaskType} from "./TaskTodoList/TaskTodoList";
+
+export type ListTasksTodoListType = {
+   todoList_ID: string
+   tasks: TaskType[]
+   removeTaskCallback: (todoList_ID: string, task_ID: string) => void
+   changeCheckedTaskCallback: (todoList_ID: string, task_ID: string, checked: boolean) => void
+}
+
+export const ListTasksTodoList: React.FC<ListTasksTodoListType> = (
+   {
+      todoList_ID,
+      tasks,
+      removeTaskCallback,
+      changeCheckedTaskCallback
+   }
+) => {
+   return (
+      <List component="nav"
+            aria-label="mailbox folders">
+         {tasks.map(task => {
+            return (
+               <TaskTodoList todoList_ID={todoList_ID}
+                             task={task}
+                             removeTaskCallback={removeTaskCallback}
+                             changeCheckedTaskCallback={changeCheckedTaskCallback}/>
+            )
+         })}
+      </List>
+   );
+};
