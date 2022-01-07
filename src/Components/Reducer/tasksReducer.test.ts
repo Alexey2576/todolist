f8eb6514d7ee911c2222258bb37cd2bb74c55b24
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-07 14:35:33 +0300

    Added tasksReducer. Unit tests for tasksReducer is done

---
+++ src/Components/Reducer/tasksReducer.test.ts
@@ -0,0 +1,82 @@
+import {
+   addTaskAC,
+   changeTaskTitleAC,
+   removeTaskAC,
+   setCheckedTaskAC,
+   tasksReducer,
+   TasksStateType
+} from "./tasksReducer";
+import {addTodoListAC, removeTodoListAC} from "./todoListReducer";
+
+
+let startState: TasksStateType
+
+beforeEach(() => {
+   startState = {
+      "todolistId1": [
+         {task_ID: "1", task_title: "React", task_priority: "Middle", checked: false},
+         {task_ID: "2", task_title: "HTML", task_priority: "All", checked: true},
+         {task_ID: "3", task_title: "CSS", task_priority: "Low", checked: false},
+      ],
+      "todolistId2": [
+         {task_ID: "1", task_title: "Bread", task_priority: "All", checked: false},
+         {task_ID: "2", task_title: "Milk", task_priority: "Low", checked: true},
+         {task_ID: "3", task_title: "Beer", task_priority: "Middle", checked: true},
+      ]
+   }
+})
+
+test('correct task should be deleted from correct array', () => {
+   const action = removeTaskAC("todolistId2", "2");
+   const endState = tasksReducer(startState, action)
+   expect(endState).toEqual({
+      "todolistId1": [
+         {task_ID: "1", task_title: "React", task_priority: "Middle", checked: false},
+         {task_ID: "2", task_title: "HTML", task_priority: "All", checked: true},
+         {task_ID: "3", task_title: "CSS", task_priority: "Low", checked: false},
+      ],
+      "todolistId2": [
+         {task_ID: "1", task_title: "Bread", task_priority: "All", checked: false},
+         {task_ID: "3", task_title: "Beer", task_priority: "Middle", checked: true},
+      ]
+   });
+});
+test('correct task should be added to array', () => {
+   const action = addTaskAC("Redux", "todolistId2", "Middle");
+   const endState = tasksReducer(startState, action)
+   expect(endState["todolistId2"].length).toBe(4)
+   expect(endState["todolistId2"][3]).toBeDefined()
+   expect(endState["todolistId2"][3].task_title).toBe("Redux")
+   expect(endState["todolistId2"][3].task_priority).toBe("Middle")
+});
+test('status of specified task should be changed', () => {
+   const action = setCheckedTaskAC("todolistId1", "3", true)
+   const endState = tasksReducer(startState, action)
+   expect(endState["todolistId1"][1].checked).toBe(true)
+   expect(endState["todolistId1"][2].checked).toBe(true)
+   expect(endState["todolistId2"][2].checked).toBe(true)
+})
+test('title of specified task should be changed', () => {
+   const action = changeTaskTitleAC("todolistId1", "2", "Redux")
+   const endState = tasksReducer(startState, action)
+   expect(endState["todolistId1"].length).toBe(3)
+   expect(endState["todolistId1"][0].task_title).toBe("React")
+   expect(endState["todolistId1"][1].task_title).toBe("Redux")
+   expect(endState["todolistId2"][1].task_title).toBe("Milk")
+})
+test('new array should be added when new todolist is added', () => {
+   const action = addTodoListAC("todolistId3")
+   const endState = tasksReducer(startState, action)
+   const keys = Object.keys(endState)
+   expect(keys.length).toBe(3 )
+   expect(endState["todolistId3"]).toBeDefined()
+   expect(endState["todolistId3"].length).toBe(0)
+})
+test('todoList should be deleted when todolist is clicked', () => {
+   const action = removeTodoListAC("todolistId1")
+   const endState = tasksReducer(startState, action)
+   const keys = Object.keys(endState)
+   expect(keys.length).toBe(1 )
+   expect(endState["todolistId1"]).not.toBeDefined()
+   expect(endState["todolistId2"].length).toBe(3)
+})
---
+++ src/Components/Reducer/tasksReducer.ts
@@ -0,0 +1,62 @@
+import {FilterPriorityTaskType} from "../../App";
+import {v1} from "uuid";
+import {addTodoListAC, removeTodoListAC} from "./todoListReducer";
+import {TaskType} from "../TodoLists/ListTasksTodoList/TaskTodoList/TaskTodoList";
+
+export type TasksStateType = {
+   [todoList_ID: string]: TaskType[]
+}
+
+type ActionType =
+   ReturnType<typeof addTaskAC>
+   | ReturnType<typeof removeTaskAC>
+   | ReturnType<typeof setCheckedTaskAC>
+   | ReturnType<typeof changeTaskTitleAC>
+   | ReturnType<typeof addTodoListAC>
+   | ReturnType<typeof removeTodoListAC>
+
+const initialState = {}
+
+export const tasksReducer = (state: TasksStateType = initialState , action: ActionType): TasksStateType => {
+   switch (action.type) {
+      case "REMOVE_TASK":
+         return {
+            ...state,
+            [action.todoList_ID]: state[action.todoList_ID].filter(t => t.task_ID !== action.task_ID)
+         }
+      case "ADD_TASK":
+         return {
+            ...state,
+            [action.todoList_ID]: [
+               ...state[action.todoList_ID],
+               {task_ID: v1(), task_title: action.title, task_priority: action.selectValue, checked: false}
+            ]
+         }
+      case "SET_CHECKED_TASK":
+         return {
+            ...state,
+            [action.todoList_ID]: state[action.todoList_ID].map(t => t.task_ID === action.task_ID ? {...t, checked: action.checked} : t)
+         }
+      case "CHANGE_TASK_TITLE":
+         return {
+            ...state,
+            [action.todoList_ID]: state[action.todoList_ID].map(t => t.task_ID === action.task_ID ? {...t, task_title: action.newTitle} : t)
+         }
+      case "ADD_TODOLIST":
+         return {
+            ...state,
+            [action.todoList_ID]: []
+         }
+      case "REMOVE_TODOLIST":
+         const copyState = {...state}
+         delete copyState[action.todoList_ID]
+         return copyState
+      default:
+         return state
+   }
+}
+
+export const addTaskAC = (title: string, todoList_ID: string, selectValue: FilterPriorityTaskType) => ({type: "ADD_TASK", title, todoList_ID, selectValue} as const)
+export const removeTaskAC = (todoList_ID: string, task_ID: string) => ({type: "REMOVE_TASK", todoList_ID, task_ID} as const)
+export const setCheckedTaskAC = (todoList_ID: string, task_ID: string, checked: boolean) => ({type: "SET_CHECKED_TASK", todoList_ID, task_ID, checked} as const)
+export const changeTaskTitleAC = (todoList_ID: string, task_ID: string, newTitle: string) => ({type: "CHANGE_TASK_TITLE", todoList_ID, task_ID, newTitle} as const)
